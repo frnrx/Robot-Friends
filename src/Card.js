3289158607d557edda2f4844418c34d9e1e1fa88
Gabriela Froener <gabriela.froener@gmail.com>
2018-05-10 11:56:26 -0300

    adding a search box and style changes

--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
 	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className='br3 pa3 ma2 grow bw2 shadow-5 robotCard' id='card'>
 			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
-			<div>
+			<div className= 'robotProps'>
 				<h2>{name}</h2>
 				<p>{email}</p>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default Card;
