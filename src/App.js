c426cdb2d5ed39f04e4b4f3ff2cc063dc34a9b01
Gabriela Froener <gabriela.froener@gmail.com>
2018-05-10 14:28:07 -0300

    adding a scroll bar on the cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots';
+import Scroll from './Scroll'
 import './App.css';
 
 
@@ -10,9 +10,15 @@ class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -24,15 +30,22 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		console.log(filteredRobots);
 
-		return(
-			<div className='tc'>
-				<h1 id='title'>RobotFriends</h1>
-				<SearchBox searchChange = {this.onSearchChange} />
-				<CardList robots= { filteredRobots } />
-			</div>
-		);
+		if (this.state.robots.lenght === 0) {
+			return <h1>Loading</h1>;
+		} 
+
+		else{
+			return(
+				<div className='tc'>
+					<h1 id='title'>RobotFriends</h1>
+					<SearchBox searchChange = {this.onSearchChange} />
+					<Scroll>
+						<CardList robots= { filteredRobots } />
+					</Scroll>
+				</div>
+			);
+		}
 	}
 }
 
